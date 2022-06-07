95c7930e8abe3a932f064dae5175dbc2dba0cd4b
Tony LaForgia <alforgia428@gmail.com>
2022-06-06 20:28:22 -0500

    added styles to test css module feature

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -3,6 +3,7 @@ import classes from "./MeetupDetail.module.css";
 
 function MeetupDetail(props) {
   return (
+    // classes allows us to use same class names across different css module files w/o error, w/ appropriate file import
     <section className={classes.detail}>
       <img src={props.image} alt={props.title} />
       <h1>{props.title}</h1>
