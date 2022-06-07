ae91bd740bf4b6c1db4459124c520ee736a2b7f0
Tony LaForgia <alforgia428@gmail.com>
2022-06-06 19:46:40 -0500

    added dummy data

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1024px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some Address",
+    description: "This is a second meetup!",
+  },
+];
+
 function HomePage() {
-  return 
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
 
 export default HomePage;
