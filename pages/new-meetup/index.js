bde4732a9bdd994b7e951c23b7494ecd9325fff9
Tony LaForgia <alforgia428@gmail.com>
2022-06-08 19:26:36 -0500

    added method, body , and headers to fetch

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -4,9 +4,17 @@ import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 function NewMeetupPage() {
   async function addMeetupHandler(enteredMeetupData) {
     // Since the API here is internal, we use /api/new-meetup to fetch the data in that file path
-    const response = await fetch('/api/new-meetup') // http://some-domain.com/abc - this path is used for an external API
+    const response = await fetch("/api/new-meetup", {
+      // http://some-domain.com/abc - this path is used for an external API
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
     console.log(enteredMeetupData);
-  
   }
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
